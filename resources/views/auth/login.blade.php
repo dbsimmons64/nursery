20a5f07b5a275a19f8c600a646cf795872c6bb42
Dave Simmons <dbsimmons64@outlook.com>
2023-09-16 06:38:18 +0100

    Added a default value to input-field for updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,23 +6,8 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        <x-input-field field="email" type="email" />
+        <x-input-field field="password" type="password" />
 
         <!-- Remember Me -->
         <div class="block mt-4">
--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -1,10 +1,11 @@
 @props([
 'field',
-'type' => 'text'
+'type' => 'text',
+'default' => null,
 ])
 
 <div class="mt-4">
 <x-input-label for="{{$field}}" :value="$field" />
-<x-text-input id="{{$field}}" class="block mt-1 w-full" type="{{$type}}" name="{{$field}}" :value="old($field)" required autofocus autocomplete="{{$field}}" />
+<x-text-input id="{{$field}}" class="block mt-1 w-full" type="{{$type}}" name="{{$field}}" :value="old($field) ?? $default" required autofocus autocomplete="{{$field}}" />
 <x-input-error :messages="$errors->get($field)" class="mt-2" />
 </div>
